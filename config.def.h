2977b1e6e869ca519979b725b9a50f4b31c1bd3e
yash3001 <yashchauhan197@gmail.com>
2020-08-12 14:00:59 +0530

    some minor changes

--- config.def.h
+++ config.def.h
@@ -155,7 +155,7 @@ static unsigned int defaultrcs = 257;
  * 7: Blinking st cursor
  * 8: Steady st cursor
  */
-static unsigned int cursorstyle = 5;
+static unsigned int cursorstyle = 1;
 static Rune stcursor = 0x2603; /* snowman (U+2603) */
 
 /*
